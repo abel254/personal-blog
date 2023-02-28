722b61ca4ffaf09a41fa3efa46cb32f1a97f275e
abel254 <abelmuema353@gmail.com>
2023-02-28 12:43:28 +0300

    adjusted blog component

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
 import "./Banner.css";
 
-const banner1 = require("../../img/banner1.webp");
-const banner2 = require("../../img/banner2.webp");
-const banner3 = require("../../img/banner3.webp");
 
 function Banner() {
   return (
     <div className="banner">
       <div className="row flex-nowrap">
         <div className="col-sm-4">
-          <img src={banner1} alt="banner1" />
+          <img src='/img/banner1.webp' alt="banner1" />
         </div>
         <div className="col-sm-4">
-          <img src={banner2} alt="banner2" />
+          <img src="/img/banner2.webp" alt="banner2" />
         </div>
         <div className="col-sm-4">
-          <img src={banner3} alt="banner3" />
+          <img src="/img/banner3.webp" alt="banner3" />
         </div>
       </div>
       <div className="sticker-container d-flex justify-content-center">
