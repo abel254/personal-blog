d69d553bf657719d6986be1f834e5d81359e0a66
abel254 <abelmuema353@gmail.com>
2023-03-02 23:30:49 +0300

    response form

--- src/components/responseForm/ResponseForm.tsx
+++ src/components/responseForm/ResponseForm.tsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import './ResponseForm.css'
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import "./ResponseForm.css";
 
 function ResponseForm() {
   return (
-   
-   <div className="container response-form">
 
-    <h1>response</h1>
-    <form>
-
-    </form>
-   </div>
- 
-  )
+    <div className="response-form">
+      <h1>Let me know what's on your mind</h1>
+      <Col className="form-container offset-lg-3" lg={6}>
+        <Row className="form-elements">
+          <Col className="form-group-container" lg={6}>
+            <div className="form-group">
+              <label>First Name</label>
+              <input type="text" />
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="form-group">
+              <label>Last Name</label>
+              <input type="text" />
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="form-group">
+              <label>Email</label>
+              <input type="text" />
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="textarea-with-arrows">
+              <label>Leave us a message...</label>
+              <textarea></textarea>
+            </div>
+          </Col>
+        </Row>
+        <button>Submit</button>
+      </Col>
+    </div>
+  );
 }
 
-export default ResponseForm
+export default ResponseForm;
