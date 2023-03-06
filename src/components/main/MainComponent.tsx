11192be223a0710f6d38009dc80c2f44de5ad5aa
abel254 <abelmuema353@gmail.com>
2023-03-06 23:11:35 +0300

    responsive blog components

--- src/components/main/MainComponent.tsx
+++ src/components/main/MainComponent.tsx
@@ -1,57 +1,54 @@
 import React from "react";
 import BlogComponent from "../blogCompoment/BlogComponent";
-import './MainComponent.css'
-import blogItems from "../../data/blog-items.json"
+import "./MainComponent.css";
+import blogItems from "../../data/blog-items.json";
 import { Col, Row } from "react-bootstrap";
 
-
 function mainComponent() {
-
-  let slicedBlogItems = blogItems.slice(0,3)
-
+  let slicedBlogItems = blogItems.slice(0, 3);
 
   return (
-     <>
-      <section id="main">
+    <>
       <main>
-      <div className="main-container">
-           <div className="main-content ms-5">
-             <Row>
+        <div className="main-container">
+          <div className="main-content">
+            <Row>
               <Col xs={12} lg={7}>
-              <div className="blogs">
-                     {
-                       slicedBlogItems.map((item)=>(
-                         <div className="blog-component" key={item.id}>
-                           <BlogComponent {...item}/>
-                         </div>
-                       ))
-                     }
-                    
-                     <button>All Posts</button>
-                 </div>
+                <Row>
+                  <Col  xs={12}>
+                    <div className="blogs">
+                      {slicedBlogItems.map((item) => (
+                        <div className="blog-component" key={item.id}>
+                          <BlogComponent {...item} />
+                        </div>
+                      ))}
+                    </div>
+                  </Col>
+                  <Col  xs={12} className='blogs-button'>
+                    <button>All Posts</button>
+                  </Col>
+                </Row>
               </Col>
               <Col xs={12} lg={5}>
-              <div className="subscribe">
-                   <div className="followup ">
-                     <h1 className="fw-bold">Let the posts come to you.</h1>
-                     <form>
-                       <div className="form-group">
-                         <label>Email</label>
-                         <input type="email" />
-                       </div>
-                       <button>Subscribe</button>
-                     </form>
-                   </div>
-                   <div className="instagram"></div>
-                 </div>
+                <div className="subscribe">
+                  <div className="followup ">
+                    <h1 className="fw-bold">Let the posts come to you.</h1>
+                    <form>
+                      <div className="form-group">
+                        <label>Email</label>
+                        <input type="email" />
+                      </div>
+                      <button>Subscribe</button>
+                    </form>
+                  </div>
+                  <div className="instagram">{/* Insta Grid  */}</div>
+                </div>
               </Col>
-             </Row>
-           </div>
-         </div>
-       </main>
-      </section>
-     </>
-    
+            </Row>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
