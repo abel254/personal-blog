78f90077b65422b8a582e99762f820c9674b8943
abel254 <abelmuema353@gmail.com>
2023-03-18 13:55:09 +0300

    navbar offcanvas

--- src/components/navbar/NavbarComponent.tsx
+++ src/components/navbar/NavbarComponent.tsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import "./NavbarComponent.css";
 import { Link, NavLink } from "react-router-dom";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Button, Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 
 function NavbarComponent() {
-  const [activeLink, setActiveLink] = useState("home");
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
 
-  const onUpdateLink = (value: string) => {
-    setActiveLink(value);
-  };
+  const handleCloseOffcanvas = () => setShowOffcanvas(false);
+  const handleShowOffcanvas = () => setShowOffcanvas(true);
 
   return (
     <>
-      <div className="nav-component">
+      {/* <div className="nav-component">
         <Navbar expand="lg">
           <Container>
             <Navbar.Brand href="#home" className="ms-5 fs-3 fw-bold">
@@ -45,6 +44,65 @@ function NavbarComponent() {
             </Navbar.Collapse>
           </Container>
         </Navbar>
+      </div> */}
+
+      <div className="nav-component">
+        <Navbar expand="lg">
+          <Navbar.Brand href="#home" className="ms-5 fs-3 fw-bold">
+            Turning Heads
+          </Navbar.Brand>
+          {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">
+                <NavLink className="nav-link fw-bold fs-6" to="/">
+                  Home
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link href="#myblog">
+                <NavLink className="nav-link fw-bold fs-6" to="/myblog">
+                  My Blog
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link href="#about">
+                <NavLink className="nav-link fw-bold fs-6" to="/about">
+                  About
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link href="#responseform" className="fw-bold fs-6">
+                <a className="nav-link fw-bold fs-6">Contact</a>
+              </Nav.Link>
+            </Nav>
+            <Button variant="outline-primary" onClick={handleShowOffcanvas}>
+              Menu
+            </Button>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Offcanvas show={showOffcanvas} onHide={handleCloseOffcanvas} placement="end" className="offcanvas-max-width-600">
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="flex-column">
+              <Nav.Link href="#home" onClick={handleCloseOffcanvas}>
+                <NavLink className="nav-link fw-bold fs-6" to="/">
+                  Home
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link href="#my-blog" onClick={handleCloseOffcanvas}>
+                <NavLink className="nav-link fw-bold fs-6" to="/myblog">
+                  My Blog
+                </NavLink>
+              </Nav.Link>
+              <Nav.Link href="#about" onClick={handleCloseOffcanvas}>
+                <NavLink className="nav-link fw-bold fs-6" to="/about">
+                  About
+                </NavLink>
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Offcanvas>
       </div>
     </>
   );
